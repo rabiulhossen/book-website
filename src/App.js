0d14f7eda9dd2e568f99c93bf4438ed6b4579424
rabiul-hossen <rhsimon69@gmail.com>
2022-03-26 16:48:15 +0600

    json-file-update

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
+import Book from './Books/Book';
+import { useState } from 'react';
+import { useEffect } from 'react';
 
 function App() {
+  const [books,setBooks] = useState([]);
+  console.log(books);
+  useEffect(() => {
+    fetch ("../public/data.json")
+    .then(res => res.json())
+    .then((data) => setBooks(data))
+  },[])
   return (
     <div className="App">
-      <p>helllo</p>
+   
+      <Book></Book>
     </div>
   );
 }
---
+++ src/Books/Book.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Book = () => {
+    return (
+        <div>
+            <h1 className="big">Build Your Future With BOOK</h1>
+            <div>
+                <h1></h1>
+            </div>
+        </div>
+    );
+};
+
+export default Book;
